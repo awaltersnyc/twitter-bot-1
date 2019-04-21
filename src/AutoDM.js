b702c13101c8d1b8f447c78f0385e68af64f80fb
awaltersnyc <47585348+awaltersnyc@users.noreply.github.com>
2019-04-21 03:12:14 -0400

    Update AutoDM.js

--- src/AutoDM.js
+++ src/AutoDM.js
@@ -41,7 +41,7 @@ const GenerateMessage = name => {
   ];
   const d = new Date();
   const dayName = days[d.getDay()];
-  return `Hi ${name} Thanks for .... \n Happy ${dayName} 😊😊 `; // your message
+  return `Hi ${name} Thanks for following the Breezy Rack, Enjoy 20% off with Code BREEZY20 and Free Shipping now at www.breezyrack.com ! Follow our stories on Instagram @BreezyRack for Exclusive Drops that most of the time do not make it to our site before they sell out! Happy ${dayName} 😊😊 `; // your message
   // My message   return `Hi ${name} Thanks for being a part of my social media network. I'am the @PicsrushE founder,A new Online Image Editor completely with web technologies,I'm also a reactjs developer and medium blogger.\n Happy to discuss anytime 😊  \n Happy ${dayName} 😊😊 `;
 };
 
